359096777985efbc4c3b61a58aae87c386b8884d
harikarthik-s <harikarthiks15@gmail.com>
2024-03-20 00:05:11 +0530

    Restaurant Cards added

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ import ReactDOM from "react-dom";
  *   - Search
  *   - RestaurantContainer
  *      - Restaurant Cards
+ *        - Img
+ *        - Name of Res,Star rating,cusine, delivery time
  * Footer
  *   - Copyright
  *   - Links
@@ -40,11 +42,38 @@ const Header = () => {
   );
 };
 
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{ color: "black" }}>
+      <img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/b58sysfvskincfwmgoqd"
+        alt="res-logo"
+      />
+      <h3>California Burrito</h3>
+      <h4>Mexican, American, Salads, Continental, Keto, Healthy Food</h4>
+      <h4>4.6 stars</h4>
+      <h4>38 mins</h4>
+    </div>
+  );
+};
+
 const Body = () => {
   return (
     <div className="body">
-      <div className="search"></div>
-      <div className="restro-container"></div>
+      <div className="search">Search</div>
+      <div className="restro-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
     </div>
   );
 };
@@ -53,7 +82,7 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body/>
+      <Body />
     </div>
   );
 };
