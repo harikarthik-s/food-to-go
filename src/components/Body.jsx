431eebf529215d4486d728ab85aed66ab73f752b
harikarthik-s <harikarthiks15@gmail.com>
2024-03-25 21:18:15 +0530

    Shimmer UI added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,57 +1,9 @@
 import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 // import resList from "../utils/mockData";
-import logo from "../utils/logo.png"
-//Normal Variable
-let resList2 = [
-  {
-    data: {
-      id: "121603",
-      name: "Kannur Food Point",
-      cloudinaryImageId: "bmwn4n4bn6n1tcpc8x2h",
-      cuisines: ["Kerala", "Chinese"],
-      deliveryTime: 24,
-      avgRating: "3.9",
-    },
-  },
-  {
-    data: {
-      id: "229",
-      name: "Meghana Foods",
-      cloudinaryImageId: "xqwpuhgnsaf18te7zvtv",
-      cuisines: [
-        "Biryani",
-        "Andhra",
-        "South Indian",
-        "North Indian",
-        "Chinese",
-        "Seafood",
-      ],
-      deliveryTime: 16,
-      avgRating: "4.4",
-    }
-  },
-  {
-    data: {
-      id: "428",
-      name: "Biryani Pot",
-      cloudinaryImageId: "mdipoyzfzsa7n7igskht",
-      cuisines: ["North Indian", "Biryani"],
-      deliveryTime: 19,
-      avgRating: "3.9"
-    }
-  },
-  {
-    data: {
-      id: "223",
-      name: "Truffles",
-      cloudinaryImageId: "cd832b6167eb9f88aeb1ccdebf38d942",
-      cuisines: ["American", "Continental", "Desserts", "Italian"],
-      deliveryTime: 30,
-      avgRating: "4.4"
-    }
-  },
-]
+import logo from "../utils/logo.png";
+import Shimmer from "./Shimmer";
+
 //State Variable - we use React HOOK - useState(), useEffect() - utility function given by React
 // const [resList] = useState([
 //   {
@@ -112,7 +64,7 @@ const Body = () => {
   }
 
   if(restoList.length === 0) {
-    return <h1>Loading...</h1>
+    return <Shimmer/>
   }
 
   return (
---
+++ src/components/Shimmer.jsx
@@ -0,0 +1,35 @@
+const Shimmer = () =>{
+    return (
+        <div className="body">
+      <div className = "shimmer-image"></div>
+      <div className="shimmer-container">
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+    </div>
+    </div>
+    )
+}
+
+export default Shimmer;
