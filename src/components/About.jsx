10b00d080018c3c94244d6b3b5e9cde27e41a765
harikarthiks15 <harikarthiks15@gmail.com>
2024-03-28 10:41:39 +0000

    React-Router used

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About Food-to-Go</h1>
+      <p>
+        Food-to-Go is a food ordering application that allows users to order
+        food from their favorite restaurants.
+      </p>
+      <p>
+        The application is designed to be user-friendly and easy to use. Users
+        can browse the menus of restaurants, add items to their cart, and
+        checkout.
+      </p>
+      <p>
+        Food-to-Go is a great way to get your favorite food delivered to your
+        door.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,34 @@
+import "./Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-container">
+      <div className="contact-form">
+        <h1>Contact Us</h1>
+        <form>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" />
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" />
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" />
+          <button className="send-btn" type="submit">Send</button>
+        </form>
+      </div>
+      <div className="contact-info">
+        <h1>Our Contact Information</h1>
+        <p>
+          <strong>Address:</strong> 123 Main Street, Anytown, CA 12345
+        </p>
+        <p>
+          <strong>Phone:</strong> (123) 456-7890
+        </p>
+        <p>
+          <strong>Email:</strong> info@foodtogo.com
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
